e0cb947b4c356f994ecb0bcb5fe09edb83daef73
Colin Osborn <Colin.Osborn@gmail.com>
2016-02-20 22:19:53 -0700

    centaur

---
+++ Turing/1module/mythical-creatures copy/centaur.rb
@@ -0,0 +1,47 @@
+class Centaur
+  attr_reader :name,
+              :breed
+
+  def initialize(name, breed = "Palomino")
+    @name = name
+    @breed = breed
+    @cranky = false
+    @standing = true
+    @actions = 0
+  end
+
+  def shoot
+    if cranky?
+      "NO!"
+    else
+      @actions += 1
+      "Twang!!!"
+    end
+  end
+
+  def run
+    if cranky?
+      "NO!"
+    else
+      @actions += 1
+      "Clop clop clop clop!!!"
+    end
+  end
+
+  def cranky?
+    @cranky = true if @actions >= 3
+  end
+
+  def standing?
+    @standing
+  end
+
+  def sleep
+    "NO!" if standing?
+  end
+
+  def lay_down
+
+  end
+
+end
